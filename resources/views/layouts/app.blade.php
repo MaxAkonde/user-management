f04c8eeca8b6afabf9f07801a94e9b701bf4093e
MaxAkonde <devdevmaster@gmail.com>
2020-08-27 21:45:11 +0200

    add UserController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    <a href="{{ route('users.index') }}" class="dropdown-item">Liste des utilisateurs</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
